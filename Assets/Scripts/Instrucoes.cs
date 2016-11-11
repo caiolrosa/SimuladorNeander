edb0174a047f9b9b70e844bcddb35bd35ef2c3c4
Caio Rosa <caiolrosa1912@gmail.com>
2016-11-11 01:24:47 -0200

    Instruções e unidade de controle implementadas

--- Assets/Scripts/Instrucoes.cs
+++ Assets/Scripts/Instrucoes.cs
@@ -1,38 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Instrucoes : MonoBehaviour {
-	[SerializableField]
-	private int[] memoria;
+	private string[] memoria;
 	public Instrucoes ()
 	{
-		memoria = new string[256]();
+		memoria = new string[256];
 	}
 
 	// Funcao de para instrucao: ADD
-	public int ADD(int end, int acumulador) {
-		int valorMemoria;
-
+	public int ADD(int end, int valorAcumulador) {
+        int valorMemoria;
 		try {
 			// Pegando valor da memoria pos = posMemoria --- valor
 			valorMemoria = int.Parse(memoria[end]);
 		} catch(Exception error) {
-			throw new KeyNotFoundException();
-		}
+            throw new Exception("Não foi possível pegar o conteudo do endereço de memória", error);
+        }
 
 		// Somando valor da memoria com o valor do acumulador
-		int resultado = valorMemoria + acumulador;
+		int resultado = valorMemoria + valorAcumulador;
 
 		return resultado;
 	}
 
-	public void STORE(int end, int valor) {
+	public void STORE(int end, int valorAcumulador) {
 		try {
-			memoria[end] = valor.ToString();
+			memoria[end] = valorAcumulador.ToString();
 		} catch(Exception error) {
-			throw new Exception("Nao foi possivel inserir na posicao desejada. \n" + error);
-		}
+            throw new Exception("Não foi possível pegar o conteudo do endereço de memória", error);
+        }
 	}
 
 	public int LOAD(int end) {
@@ -41,30 +40,110 @@ public class Instrucoes : MonoBehaviour {
 		try {
 			valor = int.Parse(memoria[end]);
 		} catch(Exception error) {
-			throw new Exception("Nao foi possivel carregar da posicao desejada. \n" + error);
-		}
+            throw new Exception("Não foi possível pegar o conteudo do endereço de memória", error);
+        }
 
 		return valor;
 	}
 
-    public int NOT(int acumulador)
-    {
-        return ~acumulador;
-    }
-
     public string JUMP(int end)
     {
+        string conteudoMemoria;
+
+        try
+        {
+            conteudoMemoria = memoria[end];
+        }
+        catch (Exception error)
+        {
+            throw new Exception("Não foi possível pegar o conteudo do endereço de memória", error);
+        }
+
         return memoria[end];
     }
 
-    // OR e AND ainda nao estao 100% 
-    public void OR(int end, int acumulador)
+    public string JUMPN(int end, int valorAcumulador)
     {
-        return int.Parse(memoria[end]) | acumulador;
+        string conteudoMemoria;
+
+        if (valorAcumulador < 0)
+        {
+            try
+            {
+                conteudoMemoria = memoria[end];
+            } catch(Exception error)
+            {
+                throw new Exception("Não foi possível pegar o conteudo do endereço de memória", error);
+            }
+
+            return conteudoMemoria;
+        } else
+        {
+            return null;
+        }
+        
     }
 
-    public void AND(int end, int acumulador)
+    public string JUMPZ(int end, int valorAcumulador)
     {
-        return int.Parse(memoria[end]) & acumulador;
+        string conteudoMemoria;
+
+        if (valorAcumulador == 0)
+        {
+            try
+            {
+                conteudoMemoria = memoria[end];
+            }
+            catch (Exception error)
+            {
+                throw new Exception("Não foi possível pegar o conteudo do endereço de memória", error);
+            }
+
+            return conteudoMemoria;
+        } else
+        {
+            return null;
+        }
+    }
+
+    public string JUMPNZ(int end, int valorAcumulador)
+    {
+        string conteudoMemoria;
+
+        if (valorAcumulador != 0)
+        {
+            try
+            {
+                conteudoMemoria = memoria[end];
+            }
+            catch (Exception error)
+            {
+                throw new Exception("Não foi possível pegar o conteudo do endereço de memória", error);
+            }
+
+            return conteudoMemoria;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void NOT(int valorAcumulador)
+    {
+        int notAcumulador = valorAcumulador;
+        string binaryNot = Convert.ToString(notAcumulador, 2);
+    }
+
+    public void OR(int end, int valorAcumulador)
+    {
+        int orAcumulador = valorAcumulador | end;
+        string binaryNot = Convert.ToString(orAcumulador, 2);
+    }
+
+    public void AND(int end, int valorAcumulador)
+    {
+        int andAcumulador = valorAcumulador & end;
+        string binaryNot = Convert.ToString(andAcumulador, 2);
     }
 }
---
+++ Assets/Scripts/UnidadeControle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnidadeControle : MonoBehaviour {
+
+    public string stringAcumulador;
+    public void LerInstrucao(string instrucao)
+    {
+        int ValorAcumulador = int.Parse(stringAcumulador);
+
+        string[] instrucaoLida;
+        Instrucoes instrucoes = new Instrucoes();
+
+        try
+        {
+            instrucaoLida = instrucao.Split(' ');
+        } catch(Exception error)
+        {
+            throw new Exception("Não foi possível ler a instrução", error);
+        }
+
+        switch (instrucaoLida[0])
+        {
+            case "ADD":
+                ValorAcumulador = instrucoes.ADD(int.Parse(instrucaoLida[1]), ValorAcumulador);
+                break;
+
+            case "STORE":
+                instrucoes.STORE(int.Parse(instrucaoLida[1]), ValorAcumulador);
+                break;
+
+            case "LOAD":
+                instrucoes.LOAD(int.Parse(instrucaoLida[1]));
+                break;
+
+            case "JUMP":
+                string valorMemoria = instrucoes.JUMP(int.Parse(instrucaoLida[1]));
+                LerInstrucao(valorMemoria);
+                break;
+
+            case "JUMPN":
+                string valorMemoriaN = instrucoes.JUMPN(int.Parse(instrucaoLida[1]), ValorAcumulador);
+                LerInstrucao(valorMemoriaN);
+                break;
+
+            case "JUMPZ":
+                string valorMemoriaZ = instrucoes.JUMPZ(int.Parse(instrucaoLida[1]), ValorAcumulador);
+                LerInstrucao(valorMemoriaZ);
+                break;
+
+            case "JUMPNZ":
+                string valorMemoriaNZ = instrucoes.JUMPNZ(int.Parse(instrucaoLida[1]), ValorAcumulador);
+                LerInstrucao(valorMemoriaNZ);
+                break;
+
+            case "NOT":
+                instrucoes.NOT(ValorAcumulador);
+                break;
+
+            case "OR":
+                instrucoes.OR(int.Parse(instrucaoLida[1]), ValorAcumulador);
+                break;
+
+            case "AND":
+                instrucoes.AND(int.Parse(instrucaoLida[1]), ValorAcumulador);
+                break;
+
+            case "HALT":
+                string halt = "Programa finalizado";
+                break;
+
+            case "NOP":
+                break;
+
+            default:
+                break;
+        }
+    }
+}
